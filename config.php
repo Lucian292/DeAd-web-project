3c90fad64daffcf510349930f695e982e856b655
Lucian <lucian_munteanu@mail.ru>
2024-02-13 20:40:28 +0200

    final

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+
+// Typical configuration file
+//return an array of configuration settings
+
+$config = array(
+    'hostname' => '127.0.0.3:3306',
+    'username' => 'root',
+    'password' => 'root',
+    'database' => 'dead',
+    'secret_key' => 'bGS6lzFqvvSQ8ALbOxatm7/Vk7mLQyzqaS34Q4oR1ew='
+);
+return $config;
+?>
